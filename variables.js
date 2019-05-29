fdcfe56f6b436f84056aea2d68aeef29d4fffc97
William Hughes <4hughw39@solent.ac.uk>
2019-05-29 13:40:04 +0000

    varaibles.js

---
+++ variables.js
@@ -0,0 +1,21 @@
+//VARIABLES
+
+//Variables are simply containers that store values
+//They follow this pattern:
+var yourVariableName = yourValue;
+
+//They can store all of the values we've seen
+var name = "Will";
+var secretNumber = 29;
+var isWorking = true;
+
+//Recall the stored value by calling the variable name
+var name = "Will";
+"hello there " + name    //"hello there Will"
+
+var num = 10;
+num + 8 + 10    //28
+
+//We can also update existing variables
+var name = "Jamie";
+name = "Tyrion";
