8518d8c15bd2b315b0b2f5d47044f2a0ada53b92
William Hughes <4hughw39@solent.ac.uk>
2019-05-29 13:37:07 +0000

    functions_arguments.js

---
+++ functions_arguments.js
@@ -0,0 +1,15 @@
+//FUNCTIONS | ARGUMENTS
+
+//Functions can have as many arguments as needed
+function area(length, width) {
+    console.log(length * width);
+}
+area(9, 2); //18
+
+
+function greet(person1, person2, person3) {
+    console.log("hi " + person1);
+    console.log("hi " + person2);
+    console.log("hi " + person3);
+}
+greet("Harry", "Ron", "Hermione");
