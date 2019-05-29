0d9b19e20dcb5c673f9d81860353d0f6a7685e49
William Hughes <4hughw39@solent.ac.uk>
2019-05-29 13:21:22 +0000

    for_loops.js

---
+++ for_loops.js
@@ -0,0 +1,19 @@
+//FOR LOOPS
+//Another way of repeating code
+
+for(init; condition; step) {
+  //run some code
+}
+
+//Printing numbers from 1-5 with a for loop
+for(var count = 0; count < 6; count++) {
+  console.log(count);
+}
+
+//Printing numbers from 1-5 with a while loop
+var count = 1;
+
+while(count < 6) {
+ console.log("count is: " + count);
+ count++;
+}
