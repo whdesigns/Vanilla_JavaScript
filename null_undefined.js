360bd230037d5ddb8da1f65e9614495ce59bfd0c
William Hughes <4hughw39@solent.ac.uk>
2019-05-29 13:38:39 +0000

    null_undefined.js

---
+++ null_undefined.js
@@ -0,0 +1,11 @@
+//The two other primitives are null and undefined
+
+//Variables that are declared but not 
+//initialized are undefined
+//The following variables are undefined, but they do exist:
+var name;
+var age;
+
+//null explicitly means nothing.
+var currentPlayer = "charlie";
+currentPlayer = null;   //game over
