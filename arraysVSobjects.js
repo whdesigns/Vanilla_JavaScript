0cbfc2cc7d7c6efca56370120587ed3b571bb469
William Hughes <4hughw39@solent.ac.uk>
2019-06-08 12:09:23 +0000

    arraysVSobjects.js

---
+++ arraysVSobjects.js
@@ -0,0 +1,21 @@
+//...........O B J E C T S   V S   A R R A Y S.........//
+
+
+//.... A R R A Y S....//
+var lannisters = ["Cersei", "Tywin", "Jaimie", "Tyrion"];
+
+//....T y p i n g   l a n n i s t e r s   r e v e a l s  ["Cersei", "Tywin", "Jaimie", "Tyrion"]....//
+//....T y p i n g   l a n n i s t e r s[0]   r e v e a l s  "Cersei"....//
+
+
+//.... O B J E C T S...//
+
+//....T y p i n g   b i o   s h o w s   c o m p l e t e   i n f o....//
+
+var bio = {
+	name: "Cersei",
+	house: "Lannister",
+	sigil: "Lion",
+	houseWords: "Hear me roar"
+}
+
